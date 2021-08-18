e1039f701f9f809095489c03cad2a1748e843534
keeYuc <52500690@qq.com>
2021-08-18 11:03:28 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,24 @@ use std::io::{self, Read};
 use std::sync::mpsc;
 use std::thread;
 
+fn handle(str: String) -> String {
+    str
+}
+
 fn main() {
     let (tx, rx) = mpsc::channel();
     thread::spawn(move || loop {
         let mut buf = String::new();
-        io::stdin().read_to_string(&mut buf).unwrap();
-        tx.send(buf).unwrap();
+        io::stdin().read_to_string(&mut buf).expect("1");
+        match tx.send(buf) {
+            Ok(_) => {}
+            Err(_) => {
+                println!("err");
+                break;
+            }
+        }
     });
     for item in rx.recv() {
-        println!("{}", item)
+        println!("{}", handle(item));
     }
 }
-
